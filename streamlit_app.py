a5ef8fa75f3df3bb81f4f9c7f1d253b324b8397e
Pawel Mtw <60775082+ds-agent7@users.noreply.github.com>
2021-06-06 16:46:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,149 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+import pickle
+from _curses import beep
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import os
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import tones as tones
+from sklearn.preprocessing import StandardScaler
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.header('Предсказание прихвата на буровой')
+st.subheader('время сигнала около 120 сек')
+# загрузка csv файла:
+#@st.cache()
+w = st.file_uploader("Upload a CSV file", type="csv")
+if w:
+    import pandas as pd
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    data = pd.read_csv(w)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    import os
+
+    duration = 1  # seconds
+    freq = 440  # Hz
+    os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
+    #st.write(data)  вывод датасета на экран
+
+#file = "ctboost_predict_model60_6_New.pkl"
+#file = "https://github.com/ds-agent7/Clovery/blob/main/ctboost_predict_model60_6_New.pkl"
+#file = "https://drive.google.com/file/d/1rZ8BpoFMjh66tUNuEi9NEwQnOn8ieFUb/view?usp=sharing"
+
+    file = "ctboost_predict_model60_6_New.pkl"
+
+    pickle_in = open(file, 'rb')
+    ctboost_model = pickle.load(pickle_in)
+
+    X =data[['GR', 'SPPA_APRS', 'ECD', 'APRS', 'RPM', 'FLWI','STOR', 'BPOS', 'SPPA',
+          'DDEPT_3', 'DDEPT_6', 'conner',
+        'F', 'PDEPT', 'DDEPT_12', 'DDEPT_18','DBPOS_3','DBPOS_6', 'DBPOS_12', 'DBPOS_18', 'DEPT']].values
+
+    # нормализация:
+    sc= StandardScaler()
+    X = sc.fit_transform(X)
+    y = data['StuckPipe60_6'].values
+
+    y_predict = ctboost_model.predict(X)
+    y_predict_proba = ctboost_model.predict_proba(X)
+    df7 = pd.DataFrame(y_predict)
+    data["predict_class"] = df7
+
+
+    # Отрисовка временных рядов:
+
+    def plt_time_shift(data, time_col, col_list, hole_id):
+
+        for i in range(len(col_list)):
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            x = data[time_col]
+            y1 = data[col_list[i]]
+            y2 = data["predict_class"]  # лучше заменить на y_predict!!!
+
+            # Plot Line1 (Left Y Axis)
+            fig, ax1 = plt.subplots(1, 1, figsize=(20, 10), dpi=50)
+            ax1.plot(x, y1, color='tab:red')
+
+            ax2 = ax1.twinx()  # добавляем вторую ось у на ту же ось х:
+            ax2.plot(x, y2, color='tab:blue')
+
+            # Левая шкала у1
+            ax1.set_xlabel('Time', fontsize=40)
+            ax1.tick_params(axis='x', rotation=90, labelsize=25)
+            ax1.set_ylabel(f'{col_list[i]}', color='tab:red', fontsize=30)
+            ax1.tick_params(axis='y', rotation=0, labelcolor='tab:red')
+            ax1.grid(alpha=.4)
+
+            # Правая шкала у2
+            ax2.set_ylabel("Сигнал прихвата", color='tab:blue', fontsize=30)
+            ax2.tick_params(axis='y', labelcolor='tab:blue')
+            ax2.set_xticks(np.arange(0, len(x), 60))
+            ax2.set_xticklabels(x[::60], rotation=90, fontdict={'fontsize': 40})
+            ax2.set_title(f'Зависимость {col_list[i]} от Сигнала по скважине №{hole_id}',
+                          fontsize=40)
+            fig.tight_layout()
+            #plt.show()
+            st.pyplot()
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+    data = data[:190] #186 #500
+    X = X[:190]
+
+
+    n = data.index.max()
+    X_valid = X[n]
+    feat_list = [ 'BPOS','HKLD','STOR','ECD',"FLWI","RPM","SPPA"]
+    Time = data["Date/Time"][n]
+    hole_number = data["hole"][n]
+    id_index = data.index[n]
+    Stuckpipe = data["StuckPipe"][n]
+
+    #y_predict = ctboost_model.predict(X_valid)  # вариант с категорией
+    y_predict_proba = ctboost_model.predict_proba(X[n]) # вариант с вероятностью
+    #if y_predict==1:
+    if y_predict_proba[1] > 0.5:
+        os.system('say "Опасность прихвата через 2 минуты!"')
+        st.header ("Внимание! Вероятность прихвата: {0:0.2f}".format(
+                y_predict_proba[1]))
+
+        st.write((Time))
+        st.write("")
+        #st.write("Разметка Stuckpipe: {}".format(Stuckpipe))
+        st.write("ID: {}".format(id_index))
+        st.write("Hole: {}".format(hole_number))
+        st.write("BPOS: {0:0.2f}".format(X_valid[2]))
+        st.write("HKLD: {0:0.2f}".format(X_valid[3]))
+        st.write("STOR: {0:0.2f}".format(X_valid[4]))
+        st.write("FLWI: {0:0.2f}".format(X_valid[5]))
+        st.write("RPM: {0:0.2f}".format(X_valid[6]))
+        st.write("SPPA: {0:0.2f}".format(X_valid[7]))
+        st.write("ECD: {0:0.2f}".format(X_valid[8]))
+        st.write("")
+        st.write("")
+        st.write("")
+        time.sleep(0.1) # время задержки
+
+
+    else:
+        os.system('say "Норма"')
+        st.subheader("Показания датчиков в норме.")
+        # out_green("Норма. Вероятность прихвата: {0:0.2f}".format(
+        # y_predict_proba[1]))
+        st.write("ID: {}".format(id_index))
+        #st.write("Разметка Stuckpipe: {}".format(Stuckpipe))
+        st.write(Time)
+        st.write("")
+        st.write("")
+
+    for hole in data.hole.unique():
+        plt_time_shift(data, 'Date/Time', feat_list, hole)
